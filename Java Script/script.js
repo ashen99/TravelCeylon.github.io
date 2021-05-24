06a34d7789fb4a4980a9581953a7dd34150aad60
ashane perera <ashenperera51@yahoo.com>
2021-05-24 12:36:16 +0530

    update

--- Java Script/script.js
+++
@@ -1,6 +0,0 @@
-function colorbackground()
-{
-    var x=document.getElementById("selectid");
-    var backgroundcolor=x.options[x.selectedIndex].value;
-    document.body.style.backgroundColor=backgroundcolor;
-}
